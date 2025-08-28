ed966428467d42aded2afb2a219898da392ae9f0
SagarNegi10 <sagarnegi13.0@gmail.com>
2025-08-28 11:19:56 +0530

    Clerk login & signup

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
+import { MenuIcon, SearchIcon, TicketIcon, XIcon } from 'lucide-react'
 import { useState } from 'react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 const Navbar = () => {
 
   const [isOpen, setIsOpen] = useState(false)
+  const {user} = useUser()
+  const {openSignIn} = useClerk()
+  const navigate = useNavigate()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -17,14 +21,22 @@ const Navbar = () => {
         <XIcon onClick={() => setIsOpen(!isOpen)} className='md:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer'/>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Home</Link>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/movies'>Movies</Link>
-        <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Threaters</Link>
+        <Link onClick={() => {scrollTo(0,0); setIsOpen(false)}} to='/theaters'>Theaters</Link>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/'>Releases</Link>
         <Link onClick={() => {scrollTo(0,0), setIsOpen(false)}} to='/favorite'>Favorites</Link>
       </div>
 
       <div className='flex items-center gap-8'>
         <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-        <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+        {!user ? (
+          <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action label='My Bookings' labelIcon={<TicketIcon width={15}/>} onClick={() => navigate('/my-bookings')}/>
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
 
       <MenuIcon onClick={() => setIsOpen(!isOpen)} className='max-md:ml-4 md:hidden w-8 h-8 cursor-pointer'/>
